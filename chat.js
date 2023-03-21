55185596e417fbd180d3c265ce5af88a2028bc68
Nekomaster12 <artemmozorow@gmail.com>
2023-03-21 19:53:34 +0300

    Виртуализация готова. Когда-нибудь отрефакторю код!

--- chat.js
+++ chat.js
@@ -4,10 +4,17 @@ import Cookies from "js-cookie";
 import format from "date-fns/format";
 import { authorizationCode, EMPTY } from "./const.js";
 import { getMyData } from "./authorization.js";
+const vitrualizate = chatVirtualization()
 if(Cookies.get(authorizationCode) ){
-    showChatHistory()
+    vitrualizate().then(scrollDown)
+    
 }
 
+CHAT_ELEMENTS.MESSAGE_HISTORY.addEventListener("scroll", async function(){
+    if(CHAT_ELEMENTS.MESSAGE_HISTORY.scrollTop === 0){
+        vitrualizate()
+    }
+})
 
 function checkMessage(){
     if(CHAT_ELEMENTS.MESSAGE_INPUT.value.length > 0){
@@ -37,6 +44,7 @@ function sendMyMessage(event){
     event.preventDefault()
     socket.send(JSON.stringify({ text: CHAT_ELEMENTS.MESSAGE_INPUT.value}));
     renderMyMessage()
+    scrollDown()
 }
 
 function renderMyMessage(message = CHAT_ELEMENTS.MESSAGE_INPUT.value){
@@ -50,7 +58,7 @@ function renderMyMessage(message = CHAT_ELEMENTS.MESSAGE_INPUT.value){
     }catch(error){
         alert(error)
     }
-    scrollDown()
+   
     clearMessageInput()
 }
 
@@ -106,19 +114,49 @@ async function getChatHistory(){
     return (await response).json()
 }
 
-async function showChatHistory(){
-    const chatHisory = await getChatHistory()
-    const myData = await getMyData()
-    for(let message of await chatHisory.messages){
-        if(message.user.email !== myData.email){
-        CHAT_ELEMENTS.MESSAGE_HISTORY.prepend(createOthersMessage(message))
-        }else{
-            renderMyMessage(message.text)
+function chatVirtualization(){
+    let counter = 0
+    return async function(){
+        const chatHisory = await getChatHistory()
+
+        const myData = await getMyData()
+        if(counter >= await chatHisory.messages.length){
+            alert("Вы загрузили все сообщения")
+            return
         }
+        for(let message of await chatHisory.messages){
+            if(counter !== await chatHisory.messages.indexOf(message)){
+                continue
+            }
+            counter += 1;
+            if(counter % 20 == 0){
+                return;
+            }
+            if(message.user.email !== myData.email){
+            CHAT_ELEMENTS.MESSAGE_HISTORY.prepend(createOthersMessage(message))
+            }else{
+                renderMyMessage(message.text)
+            }
+        }
+        CHAT_ELEMENTS.DIALOGUE.prepend(CHAT_ELEMENTS.MESSAGE_HISTORY)
         
-    }
-    CHAT_ELEMENTS.DIALOGUE.prepend(CHAT_ELEMENTS.MESSAGE_HISTORY)
-    scrollDown()
-}
+    }}
+
+
+// async function showChatHistory(counter){
+//     const chatHisory = await getChatHistory()
+//     console.log(chatHisory)
+//     const myData = await getMyData()
+//     for(let message of await chatHisory.messages){
+//         if(message.user.email !== myData.email){
+//         CHAT_ELEMENTS.MESSAGE_HISTORY.prepend(createOthersMessage(message))
+//         }else{
+//             renderMyMessage(message.text)
+//         }
+        
+//     }
+//     CHAT_ELEMENTS.DIALOGUE.prepend(CHAT_ELEMENTS.MESSAGE_HISTORY)
+//     scrollDown()
+// }
 
 CHAT_ELEMENTS.MESSAGE_FORM.addEventListener("submit", function(event){sendMyMessage(event)})
